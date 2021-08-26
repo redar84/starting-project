6f19feb0e18e3b66be22c8839e5236c8f40b91fb
Redar ChiCho <redar_chicho@hotmail.com>
2021-08-25 21:47:35 -0500

    new functions

--- src/components/userform/AddUserForm.js
+++ src/components/userform/AddUserForm.js
@@ -18,7 +18,7 @@ export default function AddUserForm() {
             userName : enteredUserName,
             age: enteredAge
         }
-        console.log(newUser);
+        
         setEnteredUserName('');
         setEnteredAge('');
     }
